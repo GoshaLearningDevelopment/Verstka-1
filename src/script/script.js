e66301f9ca96b35b608396af82d33041367ce31c
Benis <53756956+YellowSubmarine2000@users.noreply.github.com>
2021-11-26 00:31:53 +0700

    Adaptation to 1024x720px version
    Коль, посмотри пожалуйста откуда ползунок широты появляется. Он даже в 4к версии почему- то есть. И в гугле svg в promo секции не отображаются почему- то, в фаерфоксе все работает и во всех остальных секциях тоже все svg на месте.

--- src/script/script.js
+++ src/script/script.js
@@ -1,1 +1,7 @@
-console.log("Hello world!")
+function button() {
+    var name = document.getElementById('name').value;
+    var mail1 = document.getElementById('mail').value;
+    var request = document.getElementById('request').value;
+
+    alert("Имя: "+name+"\nE-mail: "+mail1+"\nСообщение: "+request);
+}
